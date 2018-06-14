c8ac07aa07022655067be97bad06c91425129b01
Kso <kasuo.takumi@gmail.com>
2018-06-14 12:34:30 +0900

    update output.php 脆弱性対策
    SQLインジェクションとXSS対策をした

--- web/output.php
+++ web/output.php
@@ -1,8 +1,8 @@
 <?php
 
 //SQLログイン用情報
-$db_user = "user";
-$db_pass = "pass";
+$db_user = "root";
+$db_pass = "";
 $db_host = "localhost";
 $db_name = "kankore";
 $db_type = "mysql";
@@ -12,7 +12,7 @@ $dbh = new PDO("$db_type:hots=$db_host;dbname=$db_name;charset=utf8",$db_user,$d
 
 $shipName = $_POST["shipNameText"];
 
-//var_dump($dbh);　errorが起きたときに使える
+//var_dump($dbh);　errorが起きたときに使える?
 
 
 //SQL
@@ -20,18 +20,37 @@ $shipName = $_POST["shipNameText"];
 
 
 //艦名と艦種を取り出して $name_type_result に格納
-$name_type = $dbh->prepare("SELECT ships.id, ships.name, types.type_name FROM ships INNER JOIN types ON ships.type_id = types.id WHERE ships.name = '$shipName'");
+// プリペアドステートメントの作成
+$name_type = $dbh->prepare('SELECT ships.id, ships.name, types.type_name FROM ships INNER JOIN types ON ships.type_id = types.id WHERE ships.name = :shipName;');
+
+// 値のバインド
+$name_type->bindValue(':shipName', $shipName);
+
+// SQLの実行
 $name_type->execute();
+
 $name_type_result = $name_type->fetchALL(PDO::FETCH_ASSOC);
+
 //取得したships.idを変数に入れる
 $ship_id = $name_type_result[0]["id"];
 
+
+
+
 //取り出した艦名から出現エリアを取得し $ship_area_result に格納
-$ship_area = $dbh->prepare("SELECT drops.ship_id, areas.name FROM drops INNER JOIN areas ON drops.area_id = areas.id WHERE drops.ship_id = '$ship_id'");
+// プリペアドステートメントの作成
+$ship_area = $dbh->prepare('SELECT drops.ship_id, areas.name FROM drops INNER JOIN areas ON drops.area_id = areas.id WHERE drops.ship_id = :ship_id');
+
+// 値のバインド
+$ship_area->bindValue(':ship_id', $ship_id);
+
+// SQLの実行
 $ship_area->execute();
 $ship_area_result = $ship_area->fetchALL(PDO::FETCH_ASSOC);
 
+
 /*====================================================================================================================================================*/
+
 
 
 //出力確認用
@@ -57,24 +76,34 @@ $pdo = null;
     <main>
       <div id="output">
         <h2>検索結果</h2>
-        <p>艦名　:<?php print $name_type_result[0]["name"]; ?></p>
-        <p>艦種　:<?php print $name_type_result[0]["type_name"]; ?></p>
+        <p>艦名 : <?php print htmlspecialchars(@$name_type_result[0][name], ENT_QUOTES, 'UTF-8'); ?></p>
+        <p>艦名 : <?php print htmlspecialchars(@$name_type_result[0][type_name], ENT_QUOTES, 'UTF-8'); ?></p>
         <p>＝＝＝ドロップエリア＝＝＝</p>
-        <?php print @$ship_area_result[0][name]; ?><br />
-        <?php print @$ship_area_result[1][name]; ?><br />
-        <?php print @$ship_area_result[2][name]; ?><br />
-        <?php print @$ship_area_result[3][name]; ?><br />
-        <?php print @$ship_area_result[4][name]; ?><br />
-        <?php print @$ship_area_result[5][name]; ?><br />
-        <?php print @$ship_area_result[6][name]; ?><br />
-        <?php print @$ship_area_result[7][name]; ?><br />
-        <?php print @$ship_area_result[8][name]; ?><br />
-        <?php print @$ship_area_result[9][name]; ?><br />
-        <?php print @$ship_area_result[10][name]; ?><br />
-        <?php print @$ship_area_result[11][name]; ?><br />
-        <?php print @$ship_area_result[12][name]; ?><br />
-        <?php print @$ship_area_result[13][name]; ?><br />
-        <?php print @$ship_area_result[14][name]; ?>
+        <?php print htmlspecialchars(@$ship_area_result[1][name], ENT_QUOTES, 'UTF-8'); ?><br />
+        <?php print htmlspecialchars(@$ship_area_result[2][name], ENT_QUOTES, 'UTF-8'); ?><br />
+        <?php print htmlspecialchars(@$ship_area_result[3][name], ENT_QUOTES, 'UTF-8'); ?><br />
+        <?php print htmlspecialchars(@$ship_area_result[4][name], ENT_QUOTES, 'UTF-8'); ?><br />
+        <?php print htmlspecialchars(@$ship_area_result[5][name], ENT_QUOTES, 'UTF-8'); ?><br />
+        <?php print htmlspecialchars(@$ship_area_result[6][name], ENT_QUOTES, 'UTF-8'); ?><br />
+        <?php print htmlspecialchars(@$ship_area_result[7][name], ENT_QUOTES, 'UTF-8'); ?><br />
+        <?php print htmlspecialchars(@$ship_area_result[8][name], ENT_QUOTES, 'UTF-8'); ?><br />
+        <?php print htmlspecialchars(@$ship_area_result[9][name], ENT_QUOTES, 'UTF-8'); ?><br />
+        <?php print htmlspecialchars(@$ship_area_result[10][name], ENT_QUOTES, 'UTF-8'); ?><br />
+        <?php print htmlspecialchars(@$ship_area_result[11][name], ENT_QUOTES, 'UTF-8'); ?><br />
+        <?php print htmlspecialchars(@$ship_area_result[12][name], ENT_QUOTES, 'UTF-8'); ?><br />
+        <?php print htmlspecialchars(@$ship_area_result[13][name], ENT_QUOTES, 'UTF-8'); ?><br />
+        <?php print htmlspecialchars(@$ship_area_result[14][name], ENT_QUOTES, 'UTF-8'); ?><br />
+        <?php print htmlspecialchars(@$ship_area_result[15][name], ENT_QUOTES, 'UTF-8'); ?><br />
+        <?php print htmlspecialchars(@$ship_area_result[16][name], ENT_QUOTES, 'UTF-8'); ?><br />
+        <?php print htmlspecialchars(@$ship_area_result[17][name], ENT_QUOTES, 'UTF-8'); ?><br />
+        <?php print htmlspecialchars(@$ship_area_result[18][name], ENT_QUOTES, 'UTF-8'); ?><br />
+        <?php print htmlspecialchars(@$ship_area_result[19][name], ENT_QUOTES, 'UTF-8'); ?><br />
+        <?php print htmlspecialchars(@$ship_area_result[20][name], ENT_QUOTES, 'UTF-8'); ?><br />
+        <?php print htmlspecialchars(@$ship_area_result[21][name], ENT_QUOTES, 'UTF-8'); ?><br />
+        <?php print htmlspecialchars(@$ship_area_result[22][name], ENT_QUOTES, 'UTF-8'); ?><br />
+        <?php print htmlspecialchars(@$ship_area_result[23][name], ENT_QUOTES, 'UTF-8'); ?><br />
+        <?php print htmlspecialchars(@$ship_area_result[24][name], ENT_QUOTES, 'UTF-8'); ?><br />
+        <?php print htmlspecialchars(@$ship_area_result[25][name], ENT_QUOTES, 'UTF-8'); ?><br />
         <p></p>
       </div>
     </main>
