3953a77b5487e58f2ea2af05b2f1edbb4220e01c
Kso <kasuo.takumi@gmail.com>
2018-06-17 22:07:38 +0900

    🍣🍺

--- web/output.php
+++ web/output.php
@@ -8,7 +8,7 @@ $db_name = "kankore";
 $db_type = "mysql";
 
 //データソースネーム
-$dbh = new PDO("$db_type:hots=$db_host;dbname=$db_name;charset=utf8",$db_user,$db_pass);
+$dbh = new PDO("$db_type:hots=$db_host;dbname=$db_name;charset=utf8mb4",$db_user,$db_pass);
 
 $shipName = $_POST["shipNameText"];
 
@@ -66,7 +66,7 @@ $pdo = null;
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
+    <meta charset="utf-8">
     <title>艦これDDB</title>
     <link rel="stylesheet" href="style.css">
 </head>
@@ -77,34 +77,34 @@ $pdo = null;
     <main>
       <div id="output">
         <h2>検索結果</h2>
-        <p>艦名 : <?php print htmlspecialchars(@$name_type_result[0][name], ENT_QUOTES, 'UTF-8'); ?></p>
-        <p>艦名 : <?php print htmlspecialchars(@$name_type_result[0][type_name], ENT_QUOTES, 'UTF-8'); ?></p>
+        <p>艦名 : <?php print htmlspecialchars(@$name_type_result[0][name], ENT_QUOTES, 'utf-8'); ?></p>
+        <p>艦名 : <?php print htmlspecialchars(@$name_type_result[0][type_name], ENT_QUOTES, 'utf-8'); ?></p>
         <p>＝＝＝ドロップエリア＝＝＝</p>
-        <?php print htmlspecialchars(@$ship_area_result[1][name], ENT_QUOTES, 'UTF-8'); ?><br />
-        <?php print htmlspecialchars(@$ship_area_result[2][name], ENT_QUOTES, 'UTF-8'); ?><br />
-        <?php print htmlspecialchars(@$ship_area_result[3][name], ENT_QUOTES, 'UTF-8'); ?><br />
-        <?php print htmlspecialchars(@$ship_area_result[4][name], ENT_QUOTES, 'UTF-8'); ?><br />
-        <?php print htmlspecialchars(@$ship_area_result[5][name], ENT_QUOTES, 'UTF-8'); ?><br />
-        <?php print htmlspecialchars(@$ship_area_result[6][name], ENT_QUOTES, 'UTF-8'); ?><br />
-        <?php print htmlspecialchars(@$ship_area_result[7][name], ENT_QUOTES, 'UTF-8'); ?><br />
-        <?php print htmlspecialchars(@$ship_area_result[8][name], ENT_QUOTES, 'UTF-8'); ?><br />
-        <?php print htmlspecialchars(@$ship_area_result[9][name], ENT_QUOTES, 'UTF-8'); ?><br />
-        <?php print htmlspecialchars(@$ship_area_result[10][name], ENT_QUOTES, 'UTF-8'); ?><br />
-        <?php print htmlspecialchars(@$ship_area_result[11][name], ENT_QUOTES, 'UTF-8'); ?><br />
-        <?php print htmlspecialchars(@$ship_area_result[12][name], ENT_QUOTES, 'UTF-8'); ?><br />
-        <?php print htmlspecialchars(@$ship_area_result[13][name], ENT_QUOTES, 'UTF-8'); ?><br />
-        <?php print htmlspecialchars(@$ship_area_result[14][name], ENT_QUOTES, 'UTF-8'); ?><br />
-        <?php print htmlspecialchars(@$ship_area_result[15][name], ENT_QUOTES, 'UTF-8'); ?><br />
-        <?php print htmlspecialchars(@$ship_area_result[16][name], ENT_QUOTES, 'UTF-8'); ?><br />
-        <?php print htmlspecialchars(@$ship_area_result[17][name], ENT_QUOTES, 'UTF-8'); ?><br />
-        <?php print htmlspecialchars(@$ship_area_result[18][name], ENT_QUOTES, 'UTF-8'); ?><br />
-        <?php print htmlspecialchars(@$ship_area_result[19][name], ENT_QUOTES, 'UTF-8'); ?><br />
-        <?php print htmlspecialchars(@$ship_area_result[20][name], ENT_QUOTES, 'UTF-8'); ?><br />
-        <?php print htmlspecialchars(@$ship_area_result[21][name], ENT_QUOTES, 'UTF-8'); ?><br />
-        <?php print htmlspecialchars(@$ship_area_result[22][name], ENT_QUOTES, 'UTF-8'); ?><br />
-        <?php print htmlspecialchars(@$ship_area_result[23][name], ENT_QUOTES, 'UTF-8'); ?><br />
-        <?php print htmlspecialchars(@$ship_area_result[24][name], ENT_QUOTES, 'UTF-8'); ?><br />
-        <?php print htmlspecialchars(@$ship_area_result[25][name], ENT_QUOTES, 'UTF-8'); ?><br />
+        <?php print htmlspecialchars(@$ship_area_result[1][name], ENT_QUOTES, 'utf-8'); ?><br />
+        <?php print htmlspecialchars(@$ship_area_result[2][name], ENT_QUOTES, 'utf-8'); ?><br />
+        <?php print htmlspecialchars(@$ship_area_result[3][name], ENT_QUOTES, 'utf-8'); ?><br />
+        <?php print htmlspecialchars(@$ship_area_result[4][name], ENT_QUOTES, 'utf-8'); ?><br />
+        <?php print htmlspecialchars(@$ship_area_result[5][name], ENT_QUOTES, 'utf-8'); ?><br />
+        <?php print htmlspecialchars(@$ship_area_result[6][name], ENT_QUOTES, 'utf-8'); ?><br />
+        <?php print htmlspecialchars(@$ship_area_result[7][name], ENT_QUOTES, 'utf-8'); ?><br />
+        <?php print htmlspecialchars(@$ship_area_result[8][name], ENT_QUOTES, 'utf-8'); ?><br />
+        <?php print htmlspecialchars(@$ship_area_result[9][name], ENT_QUOTES, 'utf-8'); ?><br />
+        <?php print htmlspecialchars(@$ship_area_result[10][name], ENT_QUOTES, 'utf-8'); ?><br />
+        <?php print htmlspecialchars(@$ship_area_result[11][name], ENT_QUOTES, 'utf-8'); ?><br />
+        <?php print htmlspecialchars(@$ship_area_result[12][name], ENT_QUOTES, 'utf-8'); ?><br />
+        <?php print htmlspecialchars(@$ship_area_result[13][name], ENT_QUOTES, 'utf-8'); ?><br />
+        <?php print htmlspecialchars(@$ship_area_result[14][name], ENT_QUOTES, 'utf-8'); ?><br />
+        <?php print htmlspecialchars(@$ship_area_result[15][name], ENT_QUOTES, 'utf-8'); ?><br />
+        <?php print htmlspecialchars(@$ship_area_result[16][name], ENT_QUOTES, 'utf-8'); ?><br />
+        <?php print htmlspecialchars(@$ship_area_result[17][name], ENT_QUOTES, 'utf-8'); ?><br />
+        <?php print htmlspecialchars(@$ship_area_result[18][name], ENT_QUOTES, 'utf-8'); ?><br />
+        <?php print htmlspecialchars(@$ship_area_result[19][name], ENT_QUOTES, 'utf-8'); ?><br />
+        <?php print htmlspecialchars(@$ship_area_result[20][name], ENT_QUOTES, 'utf-8'); ?><br />
+        <?php print htmlspecialchars(@$ship_area_result[21][name], ENT_QUOTES, 'utf-8'); ?><br />
+        <?php print htmlspecialchars(@$ship_area_result[22][name], ENT_QUOTES, 'utf-8'); ?><br />
+        <?php print htmlspecialchars(@$ship_area_result[23][name], ENT_QUOTES, 'utf-8'); ?><br />
+        <?php print htmlspecialchars(@$ship_area_result[24][name], ENT_QUOTES, 'utf-8'); ?><br />
+        <?php print htmlspecialchars(@$ship_area_result[25][name], ENT_QUOTES, 'utf-8'); ?><br />
         <p></p>
       </div>
     </main>
